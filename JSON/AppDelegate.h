6fc60cc8d7dfb111c52f6a9ef6dc8c4daa589386
Sanket Bhavsar <sanket.b@BalaBaskar.local>
2015-12-22 12:23:43 +0530

    Initial Commit

---
+++ JSON/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSON
+//
+//  Created by Sanket Bhavsar on 12/22/15.
+//  Copyright (c) 2015 Sanket Bhavsar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
